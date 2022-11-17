4f2066650e0ae1131b80c6b9000fb137815dd45b
Pahevna <afpavel@inbox.ru>
2022-11-17 13:15:55 +0400

    create project

---
+++ workOut App/workOut App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workOut App
+//
+//  Created by mac on 17.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
